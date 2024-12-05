dab308b3248eece17994778a77eec1b28f98756d
Lifecam98 <tma092024@gmail.com>
2024-12-05 14:23:31 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+const startDate = '1980-01-01';
+const endDate = '2024-12-05';
+
+const historicalUrl = `http://auroraslive.io/api/v1/historical?start=${startDate}&end=${endDate}`;
+const forecastUrl = "http://auroraslive.io/api/v1/forecast"
