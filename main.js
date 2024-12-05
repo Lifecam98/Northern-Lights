5dd50f38fffc590b35df8f1cba17e6639f570721
Lifecam98 <tma092024@gmail.com>
2024-12-05 14:25:38 +0100

    setup

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const startDate = '1980-01-01';
 const endDate = '2024-12-05';
 
+// API base link https://publicapis.io/auroras-live-science-api
 const historicalUrl = `http://auroraslive.io/api/v1/historical?start=${startDate}&end=${endDate}`;
 const forecastUrl = "http://auroraslive.io/api/v1/forecast"
